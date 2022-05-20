2a691f1ead8b51304041f22190586d6e35423141
Gmmvc <67738995+Gmmvc@users.noreply.github.com>
2022-05-20 19:40:10 +0800

    Update campushoy.js

--- campushoy.js
+++ campushoy.js
@@ -1,1 +1,26 @@
+/*
+今日校园去除广告
 
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https?:\/\/mobile\.campushoy\.com\/(v7\/api\/config\/advertise\/showad) url script-response-body https://raw.githubusercontent.com/Gmmvc/quanX/main/campushoy.js
+
+[mitm]
+hostname = mobile.campushoy.com
+
+***************************/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const path = '/v7/api/config/advertise/showad';
+
+if (url.indexOf(path) != -1) {
+	obj.data = [];
+	body = JSON.stringify(obj);
+}
+
+$done({body});
